48b2fe84eaafaa98211e69c4ce7ed91e1e9063e7
pidewap <pidewap@gmail.com>
2017-04-20 08:16:15 +0700

    Update and rename satriatop.php to 480.php

---
+++ 480.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(0);
+function maling($content,$start,$end){
+if($content && $start && $end) {
+$r = explode($start, $content);
+if (isset($r[1])){
+$r = explode($end, $r[1]);
+return $r[0];
+}
+return '';
+}
+}
+$uar=array('Nokia2610/2.0 (07.04a) Profile/MIDP-2.0 Configuration/CLDC-1.1 UP.Link/6.3.1.20.0','Nokia5300/2.0 (05.51) Profile/MIDP-2.0 Configuration/CLDC-1.1','Nokia6030/2.0 (y3.44) Profile/MIDP-2.0 Configuration/CLDC-1.1','NokiaN70-1/5.0616.2.0.3 Series60/2.8 Profile/MIDP-2.0 Configuration/CLDC-1.1');
+$uarr=array_rand($uar);
+$uarand=$uar[$uarr];
+
+ini_set('default_charset',"UTF-8");
+ini_set('user_agent',$uarand."\r\naccept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\naccept_charset: $_SERVER[HTTP_ACCEPT_CHARSET]\r\naccept_language: bahasa");
+$f=file('http://keepvid.com/?url=http://www.youtube.com/watch?v='.$_GET['id'].'');
+$gg=@implode($f);
+$gg = preg_replace('/\s+/', ' ',$gg);
+$gg=str_replace('&title=', '&title=waphan.co+-+', $gg);
+$ggg=maling($gg, '<div class="d-info2">', '</dl>');
+
+$isatu=maling($ggg, '720', '480p');
+$satu=maling($isatu, '<a href="', '">');
+header('Location: '.$satu.'');
+?>
